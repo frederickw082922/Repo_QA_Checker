7c4dd7340f2ed3ceb6860b8462a2a18bdb20fb7e
Frederick Witty <frederick.witty@gotyto.com>
2026-03-09 10:50:03 -0400

    CodeQL Polynomial regular expression used on uncontrolled data fix
    Signed-off-by: Frederick Witty <frederick.witty@gotyto.com>

--- scripts/audit_compare.py
+++ scripts/audit_compare.py
@@ -47,7 +47,7 @@ def detect_benchmark_name(pre_file, post_file):
         if match:
             raw = match.group(1)
             # Format nicely: "RHEL10 CIS", "Ubuntu2204 STIG", etc.
-            inner = re.match(r'([a-zA-Z]+?)(\d{1,10})(cis|stig)', raw, re.IGNORECASE)
+            inner = re.match(r'([a-zA-Z]{1,20})(\d{1,10})(cis|stig)', raw, re.IGNORECASE)
             if inner:
                 os_name = inner.group(1).upper()
                 version = inner.group(2)
